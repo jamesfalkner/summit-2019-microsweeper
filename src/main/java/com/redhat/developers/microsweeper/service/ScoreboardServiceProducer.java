218c68d8a59c792d2d4bda161ed5464437d1bf77
jamesfalkner <schtool@gmail.com>
2019-04-25 11:34:25 -0400

    cleanup

--- src/main/java/com/redhat/developers/microsweeper/service/ScoreboardServiceProducer.java
+++ src/main/java/com/redhat/developers/microsweeper/service/ScoreboardServiceProducer.java
@@ -13,7 +13,7 @@ public class ScoreboardServiceProducer {
 
     final Logger logger = LoggerFactory.getLogger(getClass());
 
-    Optional<String> cosmosdbUri = Optional.of("mongodb://3454353434-2323-24be-2342-222c5ebadb61:NpxCEjVIe82NW3RnHxoVeD2iBUpfFOHdVRXnDpRo3oxMm3sdg==@asdf8c-81d0");
+    Optional<String> cosmosdbUri = Optional.of("localhost://3454353434-2323-24be-2342-222c5ebadb61:NpxCEjVIe82NW3RnHxoVeD2iBUpfFOHdVRXnDpRo3oxMm3sdg==@asdf8c-81d0");
 
     // @Inject
     // @ConfigProperty(name = "cosmosdb.uri")
